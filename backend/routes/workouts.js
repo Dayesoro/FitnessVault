dbd02c03c43ab268abafb362fa56547b3d9cb2e0
Daniel Ayesoro <dayesoro@gmail.com>
2023-09-08 00:24:11 +0100

    added mongodb and mongoose package
    used mongoose to create schema and model to structure the data that we want to save to the database
    
    modified the POST route to add data(workout) to the database

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import Workout from '../models/workout.model.js';
 
 const router = express.Router();
 
@@ -13,8 +14,15 @@ router.get('/:id', (req, res) => {
 });
 
 // POST a new workout
-router.post('/', (req, res) => {
-    res.json({ mssg: 'POST a new workout' })
+router.post('/', async (req, res) => {
+    const { title, load, reps } = req.body
+
+    try {
+        const workout = await Workout.create({ title, load, reps })
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 });
 
 // DELETE a workout
@@ -27,10 +35,6 @@ router.delete('/:id', (req, res) => {
 router.patch('/:id', (req, res) => {
     res.json({ mssg: 'UPDATE a workout' })
 });
-
-
-
-
 
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import workoutRoutes from './routes/workouts.js'
+import mongoose from 'mongoose';
+import workoutRoutes from './routes/workouts.js';
 import { config } from 'dotenv';
 config();
 
@@ -12,12 +13,30 @@ const app = express();
 // middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 
 // routes
 app.use('/api/workouts', workoutRoutes);
 
+const main = async () => {
+    try {
+        // Connect to the database
+        console.log(`Connecting to DB`);
+        await mongoose.connect(process.env.MONGO_URI);
 
-// listen for requests
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
+        // Start listening for requests
+        app.listen(port, () => {
+            console.log(`Connected to DB & listening on port ${port}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
+
+
